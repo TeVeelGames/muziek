d8a4a6011ed6e87f4f5db5380722f45071c39fce
TeVeelGames <39462441+TeVeelGames@users.noreply.github.com>
2018-05-21 00:10:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.TOKEN = 'NDQ3ODgyNTU3ODIwNzY0MTcw.DeOCpQ.1ZkliSxi1cDIFjUrkx4Z_L4tY0E';
 
 exports.PREFIX = '!';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyBnq7nvO4cFAhcbiqqB3njr8zj8s1iY2ms';
